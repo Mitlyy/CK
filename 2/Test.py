cdde404692751ade889093df6ee3e99da91e04b7
Mitlyy <juravelivi@gmail.com>
2022-02-27 13:33:29 +0200

    .

---
+++ 2/Test.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+report1 = pd.read_csv('2.csv', header=None, names=np.arange(shape))
+print(report1)
--- Test.py
+++ Test.py
@@ -1,17 +1,4 @@
-
-import numpy as np
-from pandas import Series, DataFrame
 import pandas as pd
+report1 = pd.read_csv('2.csv')
 
-A = {'Животное':['Собака', 'Кошка', 'Лис'],
-     'Цвет':['Белая', 'Серый', 'Рыжий'],
-     "Сила":[150, 80, 90]}
-DA = DataFrame(A, columns=['Цвет', "Сила"],
-               index=A['Животное'])
-print(DA)
-Drop = DA.drop('Собака')
-print(Drop)
-Drop.reset_index(inplace=True)
-df = Drop.rename(columns={'index':'Животное'})
-print(Drop)
-
+print(report1)
