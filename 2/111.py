b2e4c4a329046b125bb67e5bb025be0d67095aab
Mitlyy <juravelivi@gmail.com>
2022-03-05 18:00:35 +0200

    .

--- 2/111.py
+++ 2/111.py
@@ -2,20 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-report1 = pd.read_csv('.csv', header=None)
-print(report1)
-report1 = np.mat(report1)
-x = np.mat(report1)
-z = (1/len(report1)) * (x.T*x)
-print(z)
-w, v = np.linalg.eig(z)
-print(v.T[1])
-print(report1*v.T[1].T)
+Mat_sh = np.mat(pd.read_csv('mat_schet.csv',
+                            delimiter=";",
+                            header=None))
+Mat_vs = np.mat(pd.read_csv('mat_vesov.csv',
+                            delimiter=";",
+                            header=None))
 
-flip = np.flip(v, axis=1)
-g = np.dot(x, flip)
-print(g)
-print(np.shape(g))
-plt.scatter(np.array(g[:, 0]), np.array(g[:, 1]))
+Mat_vs_cr = Mat_sh*Mat_vs.T
+plt.imshow(Mat_vs_cr)
+
 plt.show()
+
 
