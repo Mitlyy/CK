f82ca4223a5f26672359bc4332c174c0884de401
Mitlyy <juravelivi@gmail.com>
2022-03-05 19:28:32 +0200

    .

--- 2/111.py
+++ 2/111.py
@@ -2,13 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-from numpy import*
+from numpy import *
 
-for i in range(1, 10):
-    temp = asarray(Image.open('Masks\1.png' %i))
 
-plt.imshow(temp*10)
+d = []
+for i in range(1, 5):
+    temp = Image.open('Masks/%d.png' % i)
+    img = temp.convert('L')
+    img = asarray(img)
+    print(img+1000)
+
+plt.imshow(img)
 
 plt.show()
-
-
