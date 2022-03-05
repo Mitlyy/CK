43be945473023b6c0218eae3e5be7a0267a2fb1c
Mitlyy <juravelivi@gmail.com>
2022-03-05 10:59:45 +0200

    .

--- 2/Test.py
+++ 2/Test.py
@@ -1,5 +1,19 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 report1 = pd.read_csv('2.csv', header=None)
 print(report1)
+d = report1.median()
+print(d)
+i = 0
+while i < len(report1.columns):
+    report1[i] = report1[i] - int(d.loc[i])
+    i += 1
+print(report1)
+x = np.mat(report1)
+print(x)
+z = (1/len(report1)) * (x.T*x)
+w, v = np.linalg.eig(z)
+
+
